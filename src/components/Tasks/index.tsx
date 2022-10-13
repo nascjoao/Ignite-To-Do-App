d6187182744bc78192b3a904be66cf469f31a5c1
nascjoao <oi@joaonasc.dev>
2022-10-13 18:44:28 -0300

    feat: style Tasks component

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -3,21 +3,23 @@ import noTasksIcon from '../../assets/no-tasks.png';
 
 export function Tasks() {
   return (
-    <section>
+    <section className={styles.tasks}>
       <header>
         <span>
           <strong>Tarefas criadas</strong>
-          <span>0</span>
+          <span className={styles.counter}>0</span>
         </span>
         <span>
           <strong>Concluídas</strong>
-          <span>0</span>
+          <span className={styles.counter}>0</span>
         </span>
       </header>
       <main>
-        <img src={noTasksIcon} aria-hidden="true" />
-        <strong>Você ainda não tem tarefas cadastradas</strong>
-        <span>Crie tarefas e organize seus itens a fazer</span>
+        <div className={styles.empty}>
+          <img src={noTasksIcon} aria-hidden="true" />
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <span>Crie tarefas e organize seus itens a fazer</span>
+        </div>
       </main>
     </section>
   )
