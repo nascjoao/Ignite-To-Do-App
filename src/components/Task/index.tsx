3e64c98456e267c74e54c98701029f6069523180
nascjoao <oi@joaonasc.dev>
2022-10-13 19:42:32 -0300

    feat: create Task component

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.css';
+import { Circle, Check, CheckCircle, Trash } from 'phosphor-react';
+
+export interface ITask {
+  content: string;
+  finished: boolean;
+}
+
+export function Task({ content, finished }: ITask) {
+  return (
+    <article className={styles.task}>
+      <button
+        title={ finished ? 'Concluída' : 'Não concluída' }
+        className={ finished ? styles.finished : styles.notFinished }
+      >
+        { finished ? (
+          <CheckCircle size={20} weight="fill" />
+        ) : (
+          <Circle size={20} weight="bold" />
+        ) }
+      </button>
+      <p className={ finished ? styles.finished : styles.notFinished }>{content}</p>
+      <button
+        title="Apagar"
+        className={styles.deleteTask}
+      >
+        <Trash size={20} />
+      </button>
+    </article>
+  )
+}
